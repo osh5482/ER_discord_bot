33fca73798eef872c5620271ff5595688b153069
osh5482 <osh5482@naver.com>
2024-04-08 16:11:56 +0900

    commit_26

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from functions.utill import *
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
 bot = commands.Bot(command_prefix="?", intents=intents)
 
 
@@ -27,6 +28,18 @@ async def on_ready():
     """봇 시작하면 로그인 로그 print하고 상태 띄워주는 함수"""
     print(f"[{current_time()}] Logged in as {bot.user}")
     await bot.change_presence(activity=discord.Game(name="실수로 눈젖빵 제작"))
+
+
+@bot.event
+async def on_guild_join(guild):
+    """새로운 서버 들어갔을때 로그띄워주는 함수"""
+    print(f"{bot.user} was invited at {guild.name}")
+
+
+@bot.event
+async def on_guild_remove(guild):
+    """서버에서 봇이 나갈 때 실행되는 코드"""
+    print(f"{bot.user} was kicked out at {guild.name}")
 
 
 @bot.command(hidden=True)
@@ -89,6 +102,16 @@ async def stop(ctx):
     await ctx.send("봇을 종료합니다.")
     print(f"[{current_time()}]Bot was stopped")
     await bot.close()
+
+
+@bot.command(hidden=True, name="서버")
+@commands.is_owner()
+async def servers(ctx):
+    servers = bot.guilds
+    server_list = [f"{server.name} (ID: {server.id})" for server in servers]
+    for i in server_list:
+        print(server_list[i])
+    print(f"cnt: {len(server_list)}")
 
 
 if __name__ == "__main__":
