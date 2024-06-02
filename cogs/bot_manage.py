e72142f749fd2062eae490f3a6de8ada85a6c5dc
osh5482 <osh5482@naver.com>
2024-06-03 01:22:06 +0900

    manage_edit

--- cogs/bot_manage.py
+++ cogs/bot_manage.py
@@ -14,13 +14,13 @@ class bot_manage(commands.Cog):
         """새로운 서버에 초대받았을 때 메시지 출력"""
         new_server = guild.system_channel
         server_info = (guild.name, guild.id)
-        if new_server:
-            await asyncio.sleep(1)
-            await new_server.send(f"눈젖빵을 {len(self.bot.guilds)}개나 만들어 버려요~")
         print(f"[{current_time()}] Bot was invited at {server_info}")
         await self.bot.change_presence(
             activity=discord.Game(name=f"눈젖빵 {len(self.bot.guilds)}개째 제작")
         )
+        if new_server:
+            await asyncio.sleep(1)
+            await new_server.send(f"눈젖빵을 {len(self.bot.guilds)}개나 만들어 버려요~")
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
@@ -37,6 +37,7 @@ class bot_manage(commands.Cog):
     async def servers(self, ctx):
         """봇이 있는 서버 리스트 출력"""
         servers = self.bot.guilds
+        total_members = sum(server.member_count for server in servers)
         server_list = [
             f"server: {server}, members: {server.member_count}" for server in servers
         ]
@@ -44,7 +45,7 @@ class bot_manage(commands.Cog):
         print(servers_str)
         print(f"[{current_time()}] server cnt: {len(server_list)}")
         await ctx.channel.send(
-            f"```{servers_str}\n 총 서버 갯수 : {len(server_list)}```"
+            f"```{servers_str}\n 총 서버 갯수 : {len(server_list)}\n 총 멤버 수: {total_members}```"
         )
 
     @commands.cooldown(1, 1800, commands.BucketType.user)
